ec2fab68d7d86b3aa9fd42872f2567a907c63668
ChrisLoukas007 <chris.dais1201@gmail.com>
2023-07-24 00:52:26 +0300

    Update user site

--- update_user.php
+++ update_user.php
@@ -95,19 +95,19 @@ if (isset($_POST['submit'])) {
                 <?php
                 $select_products = $conn->prepare('SELECT  offers.offer_id,  product.product_name, offers.product_price, product.product_image ,supermarket.supermarket_name,supermarket.supermarket_address,users.username, offers.total_likes, offers.total_dislikes FROM offers,product,supermarket,users WHERE offers.product_product_id=product.product_id AND offers.supermarket_supermarket_id=supermarket.supermarket_id AND offers.Users_user_id=users.user_id AND users.user_id = :user_id ORDER BY offer_id DESC LIMIT 6');
 
-                if (isset($_SESSION['user_id'])) {
-                    $user_id = $_SESSION['user_id'];
-                } else {
-                    $user_id = '';
-                }
+if (isset($_SESSION['user_id'])) {
+    $user_id = $_SESSION['user_id'];
+} else {
+    $user_id = '';
+}
 
-                $select_products->bindParam(':user_id', $user_id);
-                $select_products->execute();
+$select_products->bindParam(':user_id', $user_id);
+$select_products->execute();
 
-                $select_products->execute();
-                if ($select_products->rowCount() > 0) {
-                    while ($fetch_product = $select_products->fetch(PDO::FETCH_ASSOC)) {
-                ?>
+$select_products->execute();
+if ($select_products->rowCount() > 0) {
+    while ($fetch_product = $select_products->fetch(PDO::FETCH_ASSOC)) {
+        ?>
                         <form action="" method="post" class="swiper-slide slide">
                             <input type="hidden" name="pid" value="<?php echo $fetch_product['offer_id']; ?>">
                             <input type="hidden" name="name" value="<?php echo $fetch_product['product_name']; ?>">
@@ -135,17 +135,16 @@ if (isset($_POST['submit'])) {
                             </div>
                         </form>
                 <?php
-                    }
-                } else {
-                    echo '<p class="empty">Δεν υπάρχουν διαθέσιμες προσφορές!</p>';
-                }
-                ?>
+    }
+} else {
+    echo '<p class="empty">Δεν υπάρχουν διαθέσιμες προσφορές!</p>';
+}
+?>
             </div>
             <div class="swiper-pagination"></div>
         </div>
     </section>
 
-    <!-- here we create the History of like/deslike activity of user -->
     <section class="like-activity">
         <?php
         // Prepare the SQL query to retrieve the user ID
@@ -156,190 +155,193 @@ if (isset($_POST['submit'])) {
         }
 
         // Prepare the SQL query to retrieve the offer data
-        $sql = 'SELECT likeactivity.like_type AS `Like`, product.product_name AS `Product`, offers.creation_date AS `Date`
-      FROM offers
-JOIN product ON offers.product_product_id = product.product_id
-JOIN likeactivity ON offers.offer_id = likeactivity.offers_offer_id
-WHERE likeactivity.Users_user_id = ?
-ORDER BY offers.offer_id DESC LIMIT 6';
-        $stmt = $conn->prepare($sql);
-        $stmt->bindParam(1, $user_id);
+        $sql = 'SELECT 
+                likeactivity.like_type AS `Like`, 
+                product.product_name AS `Product`, 
+                users.username AS `Owner`, 
+                offers.creation_date AS `Date`
+            FROM offers
+            JOIN product ON offers.product_product_id = product.product_id
+            JOIN likeactivity ON offers.offer_id = likeactivity.offers_offer_id
+            JOIN users ON offers.Users_user_id = users.user_id
+            WHERE likeactivity.Users_user_id = ?
+            ORDER BY offers.offer_id DESC LIMIT 6';
 
-        // Execute the query
-        $stmt->execute();
+$stmt = $conn->prepare($sql); // Prepare the statement
 
-        // Create the HTML table
-        echo '<table>';
-        echo '<tr><th>Like</th><th>Product</th><th>Date</th></tr>';
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            echo '<tr><td>' . $row['Like'] . '</td><td>' . $row['Product'] . '</td><td>' . $row['Date'] . '</td></tr>';
-        }
-        echo '</table>';
-        ?>
+// Bind the user ID to the prepared statement
+$stmt->bindParam(1, $user_id);
+
+// Execute the query
+$stmt->execute();
+
+// Create the HTML table
+echo '<table>';
+echo '<tr><th>Type</th><th>Product Name</th><th>Creator of Offer</th><th>Creation Date Of Product</th></tr>';
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    echo '<tr><td>'.$row['Like'].'</td><td>'.$row['Product'].'</td><td>'.$row['Owner'].'</td><td>'.$row['Date'].'</td></tr>';
+}
+echo '</table>';
+?>
+    </section>
+
+    <!-- here we create "Total score from likes/deslikes for the user from Beginning" -->
+    <section class="score-activity">
+        <?php
+// Retrieve the user ID from the session
+if (isset($_SESSION['user_id'])) {
+    $user_id = $_SESSION['user_id'];
+} else {
+    $user_id = '';
+}
+
+// Query to calculate the total likes/deslikes score
+$sql_total = 'SELECT
+                    offers.total_likes * 5 - offers.total_dislikes AS total_likes
+                FROM offers
+                JOIN product ON offers.product_product_id = product.product_id
+                WHERE offers.Users_user_id = :user_id
+                ORDER BY product.product_id ASC';
+
+// Prepare and execute the total score query
+$stmt_total = $conn->prepare($sql_total);
+$stmt_total->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+$stmt_total->execute();
+
+// Calculate the total score for all offers
+$total_likes = 0;
+while ($row_total = $stmt_total->fetch(PDO::FETCH_ASSOC)) {
+    $total_likes += $row_total['total_likes'];
+}
+
+// Create the HTML table for the total score and product name
+echo '<table>';
+echo '<tr><th>Score from likes/dislikes</th></tr>';
+while ($row = $stmt_total->fetch(PDO::FETCH_ASSOC)) {
+    echo '<tr><td>'.$row['total_likes'].'</td><td>';
+}
+echo '<tr><td class="total-score" colspan="2">Total score for all the offers from the beginning are: '.$total_likes.'</td></tr>';
+echo '</table>';
+
+// Get the first and last day of the current month
+$first_day = date('Y-m-01');
+$last_day = date('Y-m-t');
+
+// Query to calculate the total likes/deslikes score from this month
+$sql_month = 'SELECT
+                    offers.total_likes * 5 - offers.total_dislikes AS month_likes
+                FROM offers
+                JOIN product ON offers.product_product_id = product.product_id
+                WHERE offers.Users_user_id = :user_id
+                AND offers.creation_date >= :first_day
+                AND offers.creation_date <= :last_day
+                ORDER BY product.product_id ASC';
+
+// Check if $stmt_month is not set, then prepare and execute the total score query for this month
+if (!isset($stmt_month)) {
+    $stmt_month = $conn->prepare($sql_month);
+}
+$stmt_month->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+$stmt_month->bindParam(':first_day', $first_day, PDO::PARAM_STR);
+$stmt_month->bindParam(':last_day', $last_day, PDO::PARAM_STR);
+$stmt_month->execute();
+
+// Calculate the total likes and dislikes for this month
+$month_likes = 0;
+while ($row_month = $stmt_month->fetch(PDO::FETCH_ASSOC)) {
+    $month_likes += $row_month['month_likes'];
+}
+
+// Create the HTML table for the total score and product name
+echo '<table>';
+echo '<tr><th>Score from likes/dislikes for this month</th></tr>';
+echo '<tr><td>'.$month_likes.'</td></tr>';
+echo '</table>';
+?>
     </section>
 
 
-    <!-- here we create the Scoreactivity of every user -->
+    <!-- here we create "Total score from History for the user from Beginning" -->
     <section class="score-activity">
         <?php
-        // Retrieve the user ID from the session
-        if (isset($_SESSION['user_id'])) {
-            $user_id = $_SESSION['user_id'];
-        } else {
-            $user_id = '';
-        }
-        // Query to calculate the total score and product name
-        $sql_total = 'SELECT offers.product_price, offers.total_likes, offers.total_dislikes,
-    SUM(offers.total_likes * 5 - offers.total_dislikes) AS total_score,
-    product.product_name
-    FROM offers
-    JOIN users ON offers.Users_user_id = users.user_id
-    JOIN score_activity ON score_activity.Users_user_id = offers.Users_user_id AND score_activity.offers_offer_id = offers.offer_id
-    JOIN product ON offers.product_product_id = product.product_id
-    WHERE offers.Users_user_id = :user_id
-    GROUP BY product.product_id
-    ORDER BY product.product_id ASC';
+// Initialize the variable to store the total score
+$history_cal = 0;
 
-        // Query to calculate the monthly score and product name
-        $sql_monthly = "SELECT offers.product_price, offers.total_likes, offers.total_dislikes,
-  SUM(offers.total_likes * 5 - offers.total_dislikes) AS total_score,
-  DATE_FORMAT(offers.creation_date, '%Y-%m') AS offer_month,
-  SUM(CASE WHEN DATE_FORMAT(offers.creation_date, '%Y-%m') = DATE_FORMAT(NOW(), '%Y-%m')
-      THEN offers.total_likes * 5 - offers.total_dislikes ELSE 0 END) AS monthly_score,
-  product.product_name
+// Retrieve the user ID from the session
+if (isset($_SESSION['user_id'])) {
+    $user_id = $_SESSION['user_id'];
+} else {
+    $user_id = '';
+}
+
+// Retrieve today's date with hours, minutes, and seconds
+$today = date('Y-m-d H:i:s');
+
+// Calculate the date 1 day before
+$one_day_ago = date('Y-m-d H:i:s', strtotime('-1 day', strtotime($today)));
+
+// Calculate the date 7 days ago
+$seven_days_ago = date('Y-m-d H:i:s', strtotime('-7 days', strtotime($today)));
+
+// Query to get the average price for the offers in the specified date range and product
+$sql_avg_price = 'SELECT product.product_id, AVG(offers.product_price) AS average_price
 FROM offers
-JOIN users ON offers.Users_user_id = users.user_id
-JOIN score_activity ON score_activity.Users_user_id = offers.Users_user_id AND score_activity.offers_offer_id = offers.offer_id
 JOIN product ON offers.product_product_id = product.product_id
-WHERE offers.Users_user_id = :user_id
-GROUP BY offer_month, product.product_id
-ORDER BY offer_month DESC
-LIMIT 12";
+WHERE DATE(offers.creation_date) >= :seven_days_ago
+AND DATE(offers.creation_date) <= :one_day_ago
+GROUP BY product.product_id';
 
-        // Prepare and execute the total score query
-        $stmt_total = $conn->prepare($sql_total);
-        $stmt_total->bindParam(':user_id', $user_id, PDO::PARAM_INT);
-        $stmt_total->execute();
+$stmt_avg_price = $conn->prepare($sql_avg_price);
+$stmt_avg_price->bindParam(':seven_days_ago', $seven_days_ago, PDO::PARAM_STR);
+$stmt_avg_price->bindParam(':one_day_ago', $one_day_ago, PDO::PARAM_STR);
+$stmt_avg_price->execute();
 
-        // Prepare and execute the monthly score query
-        $stmt_monthly = $conn->prepare($sql_monthly);
-        $stmt_monthly->bindParam(':user_id', $user_id, PDO::PARAM_INT);
-        $stmt_monthly->execute();
+// Initialize the history_cal variable
+$history_cal = 0;
 
-        // Fetch all the results into an array
-        $rows_total = $stmt_total->fetchAll(PDO::FETCH_ASSOC);
-        $rows_monthly = $stmt_monthly->fetchAll(PDO::FETCH_ASSOC);
+// Check each $check_offer and update history_cal accordingly
+while ($row_avg_price = $stmt_avg_price->fetch(PDO::FETCH_ASSOC)) {
+    $product_id = $row_avg_price['product_id'];
+    $average_price = $row_avg_price['average_price'];
 
-        // Calculate the total score
-        $total_score = 0;
-        foreach ($rows_total as $row) {
-            $total_score += $row['total_score'];
+    // Query to get offers that are 20% less than average_price for the specific product
+    $sql_check_offer = 'SELECT product_price, creation_date
+       FROM offers
+       WHERE Users_user_id = :user_id
+       AND product_product_id = :product_id
+       AND product_price * 0.8 <= :average_price';
+
+    $stmt_check_offer = $conn->prepare($sql_check_offer);
+    $stmt_check_offer->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+    $stmt_check_offer->bindParam(':product_id', $product_id, PDO::PARAM_INT);
+    $stmt_check_offer->bindParam(':average_price', $average_price, PDO::PARAM_INT);
+    $stmt_check_offer->execute();
+
+    // Check each $check_offer and update history_cal accordingly
+    while ($row_check_offer = $stmt_check_offer->fetch(PDO::FETCH_ASSOC)) {
+        $check_offer = $row_check_offer['product_price'];
+        $check_offer_date = $row_check_offer['creation_date'];
+
+        // Check if the offer's creation date is 1 day ago
+        $one_day_ago_check_offer = date('Y-m-d', strtotime('-1 day', strtotime($check_offer_date)));
+        if ($one_day_ago_check_offer === $one_day_ago) {
+            $history_cal += 50;
         }
 
-        // Create the HTML table for the total score and product name
-        echo '<table>';
-        echo '<tr><th>Total score from likes/dislikes</th><th>Product Name</th></tr>';
-        foreach ($rows_total as $row) {
-            echo '<tr><td>' . $row['total_score'] . '</td><td>' . $row['product_name'] . '</td></tr>';
+        // Check if the offer's creation date is up to 7 days ago
+        $seven_days_ago_check_offer = date('Y-m-d', strtotime('-7 days', strtotime($check_offer_date)));
+        if ($check_offer_date <= $seven_days_ago_check_offer) {
+            $history_cal += 20;
         }
-        echo '<tr><td class="total-score">Total score for all the offers are: ' . $total_score . '</td><td></td></tr>';
-        echo '</table>';
+    }
+}
 
-        // Create the HTML table for the monthly score and product name
-        echo '<table>';
-        echo '<tr><th>Monthly score from Likes/Dislikes</th><th>Product Name</th><th>Month</th></tr>';
-        foreach ($rows_monthly as $row) {
-            if ($row['monthly_score'] == 0) {
-                echo '<tr><td>Empty</td><td>' . $row['product_name'] . '</td><td>' . $row['offer_month'] . '</td></tr>';
-            } else {
-                echo '<tr><td>' . $row['monthly_score'] . '</td><td>' . $row['product_name'] . '</td><td>' . $row['offer_month'] . '</td></tr>';
-            }
-        }
-        echo '</table>';
-
-        ?>
-    </section>
-
-    <!-- here we create the Scoreactivity of every user -->
-    <section class="score-activity">
-        <?php
-        // Retrieve the user ID from the session
-        if (isset($_SESSION['user_id'])) {
-            $user_id = $_SESSION['user_id'];
-        } else {
-            $user_id = '';
-        }
-
-        // Query to calculate the score, product name, and creation date for offers made in the last 24 hours
-        $sql_one_day = 'SELECT offers.offer_id, offers.product_price, offers.total_likes, offers.total_dislikes,
-    product.product_name, product.subcategory_subcategory_id, offers.creation_date
-    FROM offers
-    JOIN users ON offers.Users_user_id = users.user_id
-    JOIN score_activity ON score_activity.Users_user_id = offers.Users_user_id AND score_activity.offers_offer_id = offers.offer_id
-    JOIN product ON offers.product_product_id = product.product_id
-    WHERE offers.Users_user_id = :user_id AND offers.creation_date >= DATE_SUB(NOW(), INTERVAL 1 DAY)
-    ORDER BY offers.offer_id ASC';
-
-        // Prepare and execute the query
-        $stmt_one_day = $conn->prepare($sql_one_day);
-        $stmt_one_day->bindParam(':user_id', $user_id, PDO::PARAM_INT);
-        $stmt_one_day->execute();
-
-        // Fetch all the results into an array
-        $rows_one_day = $stmt_one_day->fetchAll(PDO::FETCH_ASSOC);
-
-        // Calculate the total score
-        $total_score_one_day = 0;
-
-        // Create the HTML table
-        echo '<table>';
-        echo '<tr><th>Score</th><th>Product Name</th><th>Hour</th></tr>';
-
-        foreach ($rows_one_day as $i => $row) {
-            $score = 0;
-
-            if ($i == 0 || $row['subcategory_subcategory_id'] != $rows_one_day[$i - 1]['subcategory_subcategory_id']) {
-                // This is the first offer for this subcategory in the list, so initialize the reference price
-                $ref_price = $row['product_price'];
-                $ref_date = $row['creation_date'];
-            }
-
-            if ($row['creation_date'] > date('Y-m-d H:i:s', strtotime('-1 week'))) {
-                // Check if the offer price is less than 80% of the reference price
-                $price_ratio = $row['product_price'] / $ref_price;
-                if ($price_ratio < 0.8) {
-                    $score = 50;
-                } else if ($price_ratio < 0.9) {
-                    $score = 20;
-                }
-            } else {
-                // The offer is more than 1 week old, so assign a score of 20
-                $score = 20;
-            }
-
-            echo '<tr><td>' . $score . '</td><td>' . $row['product_name'] . '</td><td>' . $row['creation_date'] . '</td></tr>';
-            $total_score_one_day += $score;
-        }
-
-        echo '<tr><td class="total-score">Total score for the last 24 hours is: ' . $total_score_one_day . '</td><td></td><td></td></tr>';
-        echo '</table>';
-
-        ?>
-
-    </section>
-
-    <section>
-        <?php
-        $total_score_from_beginning = $total_score + $total_score_one_day;
-        echo '<section class="score-section">';
-        echo '<table>';
-        echo '<tr><th>Score activity</th></tr>';
-        echo '<tr><td class="total-score">Total score for all the offers are: ' . $total_score . '</td><td></td></tr>';
-        echo '<tr><td class="total-score">Total score for the last 24 hours is: ' . $total_score_one_day . '</td><td></td><td></td></tr>';
-        echo '<tr><td class="total-score">The score from the beginning of the record is equal to: ' . $total_score_from_beginning . '</td><td></td><td></td></tr>';
-        echo '</table>';
-        echo '</section>';
-        ?>
+// Create the HTML table for the total score and product name
+echo '<table>';
+echo '<tr><th>Score from HISTORY for the Beginning</th></tr>';
+echo '<tr><td>'.$history_cal.'</td></tr>';
+echo '</table>';
+?>
     </section>
 
 
