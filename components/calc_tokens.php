d394c3ca9b50895b216e735e8548875773940b06
Theodosis Chronopoulos <72928076+Theodoscus@users.noreply.github.com>
2023-08-27 21:15:03 +0300

    Update calc_tokens.php

--- components/calc_tokens.php
+++ components/calc_tokens.php
@@ -1,20 +1,80 @@
 <?php
 include 'components/connect.php';
-$get_tokens_cm = $conn->prepare("SELECT system_tokens_id FROM system_tokens WHERE MONTH(creation_date)=MONTH(CURDATE()) AND YEAR(creation_date)=YEAR(CURDATE())");
-$get_tokens_cm->execute();
-
-if($get_tokens_cm->rowCount() <= 0){
-    echo 'great';
-$get_users = $conn->prepare("SELECT COUNT(user_id) FROM users"); 
-$get_users->execute();
-if($get_users->rowCount() > 0){
-while($fetch_users = $get_users->fetch(PDO::FETCH_ASSOC)) {
-    $user_count = $fetch_users['COUNT(user_id)'];
+$get_min_date = $conn->prepare("SELECT MIN(DATE_FORMAT(signup_date, '%Y-%m')) AS min_date FROM users"); 
+$get_min_date->execute();
+if($get_min_date->rowCount() > 0){
+    while($fetch_min = $get_min_date->fetch(PDO::FETCH_ASSOC)) {
+        $min_date = $fetch_min['min_date'];
+    }
     
+
+$currentDate = date('Y-m-d');
+$missingMonths = array();
+$existingDates = array();
+$currentMonth = strtotime($min_date);
+$maxDate = strtotime($currentDate);
+
+while ($currentMonth< $maxDate){
+    $missingMonths[] = date('Y-m', $currentMonth);
+    $currentMonth = strtotime('+1 month', $currentMonth);
+}
+
+$get_tokens_cm = $conn->prepare("SELECT creation_date FROM system_tokens WHERE creation_date<=curdate()");
+$get_tokens_cm->execute();
+if($get_tokens_cm->rowCount() > 0){
+    while($fetch_existing_dates = $get_tokens_cm->fetch(PDO::FETCH_ASSOC)) {
+        $existingDates[] = $fetch_existing_dates['creation_date'];
+        
+    }
+    
+foreach ($missingMonths as $missingMonth) {
+    $exists = false;
+    $fullDate = $missingMonth . "-01";
+    
+    
+        foreach ($existingDates as $dates) {
+            
+            
+            if ($dates == $fullDate) {
+                $exists = true;
+                break;
+            }
+        }
+    
+       
+        if (!$exists) {
+            $get_users = $conn->prepare("SELECT COUNT(user_id) FROM users WHERE DATE_FORMAT(signup_date, '%Y-%m')<?"); 
+            $get_users->execute([$missingMonth]);
+            if($get_users->rowCount() > 0){
+            while($fetch_users = $get_users->fetch(PDO::FETCH_ASSOC)) {
+            $user_count = $fetch_users['COUNT(user_id)'];
+    
+            }
+            }
+            $tokens=$user_count*100;
+            
+            
+            $insert_system_tokens = $conn->prepare("INSERT INTO system_tokens(amount,creation_date) VALUES (?, ?)"); 
+            $insert_system_tokens->execute([$tokens, $fullDate]);
+        }
+    }
+} else {
+    foreach ($missingMonths as $missingMonth) {
+        $get_users = $conn->prepare("SELECT COUNT(user_id) FROM users WHERE DATE_FORMAT(signup_date, '%Y-%m')<?"); 
+            $get_users->execute([$missingMonth]);
+            if($get_users->rowCount() > 0){
+            while($fetch_users = $get_users->fetch(PDO::FETCH_ASSOC)) {
+            $user_count = $fetch_users['COUNT(user_id)'];
+    
+            }
+            }
+            $tokens=$user_count*100;
+            $fullDate = $missingMonth . "-01";
+            
+            $insert_system_tokens = $conn->prepare("INSERT INTO system_tokens(amount,creation_date) VALUES (?, ?)"); 
+            $insert_system_tokens->execute([$tokens, $fullDate]);
+
 }
 }
-$tokens=$user_count*100;
-echo $tokens;
-$insert_system_tokens = $conn->prepare("INSERT INTO system_tokens(amount,creation_date) VALUES (?, CURDATE())"); 
-$insert_system_tokens->execute([$tokens]);}
+}
 ?>
