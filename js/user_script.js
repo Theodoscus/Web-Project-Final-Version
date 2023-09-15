8e6eff78e4a891ddce92c3b7ceac8d30eeb5e3e0
ChrisLoukas007 <chris.dais1201@gmail.com>
2023-09-15 21:05:15 +0300

    changes

--- js/user_script.js
+++ js/user_script.js
@@ -98,3 +98,23 @@ const reviewsSlider = new Swiper(".reviews-slider", {
 
 // Close review modal
 reviewModal.style.display = "none";
+
+var swiper = new Swiper(".reviews-slider", {
+  loop: true,
+  spaceBetween: 20,
+  pagination: {
+     el: ".swiper-pagination",
+     clickable: true,
+  },
+  breakpoints: {
+     0: {
+        slidesPerView: 1,
+     },
+     768: {
+        slidesPerView: 2,
+     },
+     991: {
+        slidesPerView: 3,
+     },
+  },
+});
